d76c6d05017c063b736fc52269fd1a5b58346e9b
theDapperFoxtrot <thedapperfoxtrot.bizinq@gmail.com>
2025-02-06 16:26:32 +0200

    bug fixed

--- src/parsing/parsing_utils3.c
+++ src/parsing/parsing_utils3.c
@@ -14,7 +14,7 @@ void handle_token_append(t_ms *shell, t_command *cmd, t_token *token)
 			print_error("Error: malloc failed", shell, 1, 1);
 			exit(shell->exit_code);
 		}
-		cmd->output_file = ft_strdup(expanded_value);
+		cmd->output_file[cmd->output_count] = ft_strdup(expanded_value);
 		if (!cmd->output_file)
 		{
 			print_error("Error: malloc failed", shell, 1, 1);

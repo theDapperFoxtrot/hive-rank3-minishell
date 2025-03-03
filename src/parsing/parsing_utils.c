daad850c364d55408ff8a690d2f5650b3eacd485
theDapperFoxtrot <thedapperfoxtrot.bizinq@gmail.com>
2025-03-03 16:06:42 +0200

    norm adjustments; refactored functions

--- src/parsing/parsing_utils.c
+++ src/parsing/parsing_utils.c
@@ -92,43 +92,52 @@ void	make_heredoc_one_line(t_ms *shell, t_command *cmd)
 	cmd->heredoc_lines = NULL;
 }
 
-int event(void)
+int	event(void)
 {
 	return (0);
 }
-
-void	handle_token_heredoc(t_ms *shell, t_command *cmd, t_token *token)
+void	setup_delim(t_ms *shell, t_command *cmd, t_token *token)
 {
-	int			i;
-
-	cmd->command_input[cmd->command_input_index] = ft_strdup(token->value);
-	token = token->next;
-	if (token && token->type == TOKEN_ARGS)
+	cmd->heredoc_delimiter = ft_strdup(token->value);
+	if (!cmd->heredoc_delimiter)
 	{
-		cmd->heredoc_delimiter = ft_strdup(token->value);
-		if (!cmd->heredoc_delimiter)
-		{
-			print_error("Error: malloc failed", shell, 1, 1);
-			exit(shell->exit_code);
-		}
+		print_error("Error: malloc failed", shell, 1, 1);
+		exit(shell->exit_code);
 	}
-	i = 0;
-	sig_heredoc(&sig_handler_heredoc);
-	shell->heredoc_line_count++;
-	cmd->heredoc_lines = (char **) malloc(sizeof(char *));
+}
+
+void	allocate_heredoc_lines(t_ms *shell, t_command *cmd)
+{
+	cmd->heredoc_lines = (char **) malloc(sizeof(char *) * 2);
 	if (!cmd->heredoc_lines)
 	{
 		print_error("Error: malloc failed", shell, 1, 1);
 		exit(shell->exit_code);
 	}
 	cmd->heredoc_lines[0] = NULL;
-	i = 0;
-	rl_done = 0;
-	rl_event_hook = event;
+}
+
+int	heredoc_lines_err(t_ms *shell, t_command *cmd, int i)
+{
+	if (!cmd->heredoc_lines[i])
+	{
+		ft_putstr_fd("minishell: warning: here-document at line ", 2);
+		ft_putnbr_fd(shell->heredoc_line_count, 2);
+		ft_putstr_fd(" delimited by end-of-file (wanted `", 2);
+		ft_putstr_fd(cmd->heredoc_delimiter, 2);
+		ft_putstr_fd("')", 2);
+		ft_putstr_fd("\n", 2);
+		return (1);
+	}
+	return (0);
+}
+
+int	heredoc_loop(t_ms *shell, t_command *cmd, int i)
+{
 	while (1)
 	{
 		sig_heredoc(&sig_handler_heredoc);
-		cmd->heredoc_lines = ft_realloc(cmd->heredoc_lines, sizeof(char *) * i, sizeof(char *) * (i + 2));
+		cmd->heredoc_lines = ft_realloc(cmd->heredoc_lines, sizeof(char *) * i, sizeof(char *) * (i + 1));
 		if (!cmd->heredoc_lines)
 		{
 			print_error("Error: malloc failed", shell, 1, 1);
@@ -136,21 +145,34 @@ void	handle_token_heredoc(t_ms *shell, t_command *cmd, t_token *token)
 		}
 		cmd->heredoc_lines[i] = readline("> ");
 		if (g_signal == SIGINT)
-			return ;
-		if (!cmd->heredoc_lines[i])
-		{
-			ft_putstr_fd("minishell: warning: here-document at line ", 2);
-			ft_putnbr_fd(shell->heredoc_line_count, 2);
-			ft_putstr_fd(" delimited by end-of-file (wanted `", 2);
-			ft_putstr_fd(cmd->heredoc_delimiter, 2);
-			ft_putstr_fd("')", 2);
-			ft_putstr_fd("\n", 2);
 			break ;
-		}
+		if (heredoc_lines_err(shell, cmd, i))
+			break ;
 		if (ft_strncmp(cmd->heredoc_lines[i], cmd->heredoc_delimiter, ft_strlen(cmd->heredoc_lines[i])) == 0)
 		break ;
 		i++;
 	}
+	return (i);
+}
+
+void	handle_token_heredoc(t_ms *shell, t_command *cmd, t_token *token)
+{
+	int	i;
+
+	cmd->command_input[cmd->command_input_index] = ft_strdup(token->value);
+	token = token->next;
+	if (token && token->type == TOKEN_ARGS)
+		setup_delim(shell, cmd, token);
+	i = 0;
+	sig_heredoc(&sig_handler_heredoc);
+	shell->heredoc_line_count++;
+	allocate_heredoc_lines(shell, cmd);
+	i = 0;
+	rl_done = 0;
+	rl_event_hook = event;
+	i = heredoc_loop(shell, cmd, i);
+	if (g_signal == SIGINT)
+		return ;
 	shell->heredoc_line_count = shell->heredoc_line_count + i;
 	cmd->heredoc_lines[i] = NULL;
 	make_heredoc_one_line(shell, cmd);
